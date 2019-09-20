7f758c29e8828fdbe7ccae2918ba5a45a107ea5d
Zaya <zayazaw67@gmail.com>
2019-09-20 14:38:29 -0500

    removed somethings

--- logic.js
+++ logic.js
@@ -1,43 +1,20 @@
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://apidojo-booking-v1.p.rapidapi.com/properties/list?price_filter_currencycode=USD&travel_purpose=leisure&categories_filter=price%3A%3A9-40%2Cfree_cancellation%3A%3A1%2Cclass%3A%3A1%2Cclass%3A%3A0%2Cclass%3A%3A2&search_id=none&order_by=popularity&children_qty=2&languagecode=en-us&children_age=5%2C7&search_type=city&offset=0&dest_ids=-3712125&guest_qty=1&arrival_date=2020-01-01&departure_date=2020-01-03&room_qty=1",
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "apidojo-booking-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "4eb47b353emshb1dde063c97b955p15ac25jsn48408650620d"
-// 	}
-// }
-
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
-
-// grab inputs from html
-var inputs = {
-	// cityCode: 2810,
-	airportCode: "ATL"
-// 	pickupDate: 
-// 	pickupHour: 
-// 	dropoffDate: 
-// 	dropoffHour: 
-// 	currency: 
-}
-
-var settings2 = {
+var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://apidojo-kayak-v1.p.rapidapi.com/cars/create-session?origincitycode=6126&originairportcode=" + inputs.airportCode + "&pickupdate=2019-12-20&pickuphour=16&dropoffdate=2019-12-22&dropoffhour=6&currency=USD",
-	// "url": "https://apidojo-kayak-v1.p.rapidapi.com/cars/create-session?origincitycode=" + cityCode + "&originairportcode=" + airportCode + "&pickupdate=" + pickupDate + "&pickuphour=" + pickupHour + "&dropoffdate=" + dropoffDate + "&dropoffhour=" + dropoffHour + "&currency=" + currency,
-	"method": "GET",
+	"url": "https://googlemapsdistancematrixstefan-skliarovv1.p.rapidapi.com/getWalkingDistanceMatrix",
+	"method": "POST",
 	"headers": {
-		"x-rapidapi-host": "apidojo-kayak-v1.p.rapidapi.com",
-		"x-rapidapi-key": "4eb47b353emshb1dde063c97b955p15ac25jsn48408650620d"
+		"x-rapidapi-host": "GoogleMapsDistanceMatrixstefan-skliarovV1.p.rapidapi.com",
+		"x-rapidapi-key": "517cfaf70bmshf561bc8c9eb73e6p19dbb4jsn62aee8ad1606",
+		"content-type": "application/x-www-form-urlencoded"
+	},
+	"data": {
+		"origins": "ChIJ4Zq9gHLTD4gR2nsgOZj2p_w",
+		"destinations": "ChIJB5o6CWvTD4gR25QC-QbMQAk",
+		"apiKey": "AIzaSyBAkwDU0rJ5hbsSRO8Fx_aIFjiSMewv1oA"
 	}
 }
 
-
-
-$.ajax(settings2).done(function (response) {
+$.ajax(settings).done(function (response) {
 	console.log(response);
-});
+});	
